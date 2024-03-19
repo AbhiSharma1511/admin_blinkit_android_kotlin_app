52fcd6406e857738de0ba195fc618b89713ee776
abhi <abhi@techno.co>
2024-03-19 23:18:03 +0530

    Add Authentication fragments and work fragment for admin

---
+++ app/src/main/java/com/kotlin/adminblinkit/activity/AdminMainActivity.kt
@@ -0,0 +1,23 @@
+package com.kotlin.adminblinkit.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.kotlin.adminblinkit.R
+import com.kotlin.adminblinkit.databinding.ActivityAdminMainBinding
+
+class AdminMainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAdminMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAdminMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        // Set up the bottom navigation menu with the NavController
+        binding.bottomMenu.setupWithNavController(navController)
+    }
+}
